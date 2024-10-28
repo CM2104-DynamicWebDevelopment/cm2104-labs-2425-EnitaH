5e430d34871a22196109750c5a5ef04615e77b1c
Enita Hashemi <enitahashemi@yahoo.com>
2024-10-28 04:18:26 +0330

    4th commit

--- lab_5/errors.js
+++ lab_5/errors.js
@@ -1,12 +1,12 @@
-function main () {
-	var a b
-	var c;
-	
-	a = 10;
-	b = 100;
-	
-	b = + a b;
-	c == a * b;
-	
-	document.writeln(c);
+function pain() {
+    var a, b;
+    var c;
+
+    a = 10;
+    b = 100;
+
+    b = b + a;  // Or simply b += a;
+    c = a * b;
+
+    document.writeln(c);  // Outputs the value of c in the document
 }
