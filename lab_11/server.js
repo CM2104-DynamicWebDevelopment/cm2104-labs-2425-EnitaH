a78c00e7c3217ecf3e53d5653a8ae6265864863d
Enita Hashemi <enitahashemi@yahoo.com>
2025-02-06 15:52:03 +0330

    step5 lab2

--- lab_11/server.js
+++ lab_11/server.js
@@ -1,15 +1,22 @@
 var express = require('express'); 
 var app = express(); 
+//route1
 app.get('/', function(req, res){  
       res.send("Hello world! by express"); 
 });
 
-
+//route2
 app.get('/test', function(req, res){  
       res.send("this is route 2"); 
-    }); 
+}); 
     
-    
+//route3
+
+app.get('/add', function(req, res){ 
+    var x = req.query.x; 
+    var y = req.query.y;   
+    res.send("X + Y="+(x+y));
+ }); 
 
 
 app.listen(8080);
