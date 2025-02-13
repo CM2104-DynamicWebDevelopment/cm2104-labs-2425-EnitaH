040da495f6bdf7ed237ab98f8cdb8294b6f0e650
Enita Hashemi <enitahashemi@yahoo.com>
2025-02-13 14:22:29 +0330

    step7 lab2

--- lab_11/server.js
+++ lab_11/server.js
@@ -23,3 +23,10 @@ app.listen(8080);
 
 
 app.use(express.static('public'))
+
+app.get('/getform', function(req, res)
+{ 
+    var name = req.query.name; 
+    var quest = req.query.quest;   
+        res.send("Hi "+name+" I am sure you will "+quest) ;
+     });
